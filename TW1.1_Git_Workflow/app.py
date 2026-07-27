0579ec8942ae714c523caae8115224bc67c62f54
Shrivali <149003676+shrivalidutt@users.noreply.github.com.>
2026-07-27 13:31:50 +0530

    fix: Update route log on student branch

--- TW1.1_Git_Workflow/app.py
+++ TW1.1_Git_Workflow/app.py
@@ -4,8 +4,10 @@ app = Flask(__name__) # Create a Flask application.
 
 @app.route('/')
 def hello():
+    print("Shrivali student branch route logging active.")
     return "Hello World"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) # Start the server.
+
     
