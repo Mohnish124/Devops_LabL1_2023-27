13c6fec538d82911ec7aa56dcf34675393634cc3
Sona1147 <sonaligupta1147@gmail.com>
2026-08-01 15:20:27 +0530

    Task 1.3: Clean up conflict markers in app.py

--- 23070122266_SonaliGupta/Assignment_TW1.1_Git_Workflow/app.py
+++ 23070122266_SonaliGupta/Assignment_TW1.1_Git_Workflow/app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
     print("Conflict trigger: Main branch version.")
-=======
     print("Main branch update: Dashboard initialized.")
->>>>>>> feature/user-auth
     return "Hello, World!"
 
 if __name__ == '__main__':
