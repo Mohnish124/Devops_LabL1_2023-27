9433db6295f96fd2d5610e0a7440a0b7cf971d56
Chintnn <chintankpradhan@gmail.com>
2026-08-24 13:32:06 +0530

    Add Project 5 Spring Boot app, Dockerfile, and Trivy vulnerability scan

---
+++ 23070122077_ChintanPradhan/Project5-Spring-Boot-Docker-Scan/retail-app/src/test/java/com/chintan/retail/RetailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chintan.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
