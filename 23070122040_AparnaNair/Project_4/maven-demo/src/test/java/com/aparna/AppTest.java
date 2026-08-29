92841587283b114b909dc411a9663668a5fd92a3
aparna2004-git <nairaparna752@gmail.com>
2026-08-29 19:48:25 +0530

    Add Project 4

---
+++ 23070122040_AparnaNair/Project_4/maven-demo/src/test/java/com/aparna/AppTest.java
@@ -0,0 +1,13 @@
+package com.aparna;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testMessage() {
+        assertEquals("Hello from Maven Project 4!", 
+                     "Hello from Maven Project 4!");
+    }
+}
